11c003839677aec355aacb52ada76201d798bf34
Christian Ochei <78764474+christian-ochei@users.noreply.github.com>
2025-10-20 10:39:14 -0500

    Fix import statement for neuralengine module

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # from . import Recurrent
 # from . Network import Network,Adverserial,Batch
-from . nuralengine import *
+from . neuralengine import *
 from . Gradients import Gradients
 from . Maths.LogicUnit import *
 from . Layer import *
